3e1deb1fcfa559d0fb46cc7d4423f046ec52749d
elizeufm24 <elizeufreitasmiranda@gmail.com>
2019-03-11 13:58:17 -0300

    mensagem de boa noite adicionada e remaçõa de espaços em branco

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -5,6 +5,7 @@ public class Program {
 	public static void main(String[] args) {
 		
 		System.out.println("Good morning!");
-		System.out.println("good afternon!");
+		System.out.println("Good afternon!");
+		System.out.println("Good morning!");
 	}
 }
